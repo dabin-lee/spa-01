f6ced8b1fe3025caee121f114081168d1d379080
db <dblee@ipartners.co.kr>
2020-06-25 17:30:34 +0900

    20200625

--- js/script.js
+++ js/script.js
@@ -3,13 +3,66 @@
 
 $(function(){
     var $featureCont = $('.feature__cont'),
-        $featureContLi = $featureCont.find('> li');
+        $featureContLi = $featureCont.find('ul > li');
 
-        $featureCont.on("focus, mouseover", function(){
-            $(this).find('li').addClass('feature__block');
-            }).on("mouseout", function(){
-            $(this).find('span').removeClass('feature__block');
+        $featureContLi.each(function(i){
+            // for(var i = 0; i <= $featureContLi.length; i++);;
+            $(this).mouseenter(function(){
+                $featureContLi.removeClass('feature__block');
+                $featureContLi.eq(i).addClass('feature__block').animate({},3000);
+                $featureContLi.eq(i).find(' > span').css("display", "block");
+            })
+            $(this).mouseleave(function(i){
+                $featureContLi.removeClass('feature__block');
+                $featureContLi.find(' > span').css("display", "none");
+            });
+        });
+
+
+        // slideup
+        var $_maNager = $('.manager--profile');
+
+            $_maNager.on("mouseenter", function(){
+                if($(this).hasClass('over') == true) {
+                    $_maNager.removeClass('over');
+                }else{
+                    $(this).addClass('over');
+                }
+            });
+            $_maNager.on("mouseleave", function(){
+                $_maNager.removeClass('over');
+            });
+
+        // indicator_slide
+            var $slideGroup = $('.article__group--testimonials'),
+            $slides = $slideGroup.find('> .indicator--article'),
+            $slideCount = $slides.length;
+
+            var $inDicatorList = $('.slider__indicator--btn'),
+            $inDicatorBtn = $inDicatorList.find('> li'),
+            $currentIndex = 0,
+            $duration = 500,
+            $easing = 'easeInOutExpo',
+            $interval = 3500;
+
+
+            // 각각 slide마다 할일
+            $slides.each(function(i){
+                var newLeft = i * 100 + '%'; //100%씩 옆으로
+                $(this).css({left: newLeft});
+            });
+
+            // 슬라이드 이동 함수
+            function goToslide(index){
+                $slideGroup.animate({left: - 100 * index + '%'}, $duration, $easing);
+                $currentIndex = index;
+                }
+                console.log($slides);
+
+            $inDicatorList.find('li').click(function(e){
+                e.preventDefault();
+                var idx = $(this).index(); //클릭한 li의 index를 알기위해
+                console.log(idx);
+                goToslide(idx);
             });
     });
-
-console.log(featureCont);
