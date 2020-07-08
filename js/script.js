f2bbe1c8deeb2b281dd4657421b3ea92c7fa569c
db <dblee@ipartners.co.kr>
2020-07-08 16:10:32 +0900

    0708

--- js/script.js
+++ js/script.js
@@ -45,4 +45,19 @@ $(function(){
                 })
             })
 
+
+        // datepicker, timepicker
+        $("#reserveDate").datepicker({});
+        $('#timePicker').timepicker({
+            timeFormat: 'p h:mm',
+            interval: 60,
+            minTime: '09',
+            maxTime: '11:00pm',
+            defaultTime: '14',
+            startTime: '00:00',
+            dynamic: false,
+            dropdown: true,
+            scrollbar: true
+        });
+
     });
